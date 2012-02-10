e24025ccb9c2cb1ce3bf7a36b6c6772caefa4aa6
Ben Huson <ben@thewhiteroom.net>
2012-02-10 07:48:42 +0000

    Move to class structure and tidy code

--- list-pages-at-depth.php
+++ list-pages-at-depth.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 /*
 
 Plugin Name: List Pages at Depth
@@ -16,65 +14,75 @@ http://www.opensource.org/licenses/gpl-license.php
 
 */
 
+global $List_Pages_At_Depth;
+$List_Pages_At_Depth = new List_Pages_At_Depth();
 
-
-require_once('includes/widget.php');
-
-
-
-function list_pages_at_depth( $args = '') {
+class List_Pages_At_Depth {
 	
-	global $post;
-	
-	if ( !isset($args['startdepth']) ) {
-		$args['startdepth'] = 0;
+	/**
+	 * Constructor
+	 */
+	function List_Pages_At_Depth() {
+		require_once( dirname( __FILE__ ) . '/includes/widget.php' );
+		add_shortcode( 'list-pages-at-depth', array( $this, 'shortcode_list_pages_at_depth' ) );
 	}
 	
-	if ( is_page() || $args['startdepth'] == 0 ) {
+	/**
+	 * List pages
+	 */
+	function list_pages( $args = '' ) {
+		global $post;
 		
-		$result = array();
-		$result = list_pages_at_depth_parent( $post->ID, $result );
+		if ( ! isset( $args['startdepth'] ) )
+			$args['startdepth'] = 0;
 		
-		if ( $args['startdepth'] < count($result) ) {
-			$args['child_of'] = $result[$args['startdepth']];
-			return wp_list_pages( $args );
+		if ( is_page() || $args['startdepth'] == 0 ) {
+			$result = array();
+			$result = $this->list_pages_at_depth_parent( $post->ID, $result );
+			
+			if ( $args['startdepth'] < count( $result ) ) {
+				$args['child_of'] = $result[$args['startdepth']];
+				return wp_list_pages( $args );
+			}
 		}
+	}
+	
+	/**
+	 * List pages at depth parent
+	 */
+	function list_pages_at_depth_parent( $page_id, $result ) {
+		$page = get_page( $page_id );
 		
-	}
-}
-
-
-
-function list_pages_at_depth_parent( $page_id, $result ) {
-	
-	$page = get_page($page_id);
-	
-	if ( !in_array($page->ID, $result) ) {
-		array_unshift($result, $page->ID);
-	}
-	if ( !in_array($page->post_parent, $result) ) {
-		array_unshift($result, $page->post_parent);
+		if ( ! in_array( $page->ID, $result ) )
+			array_unshift( $result, $page->ID );
+		if ( ! in_array( $page->post_parent, $result ) )
+			array_unshift( $result, $page->post_parent );
+		
+		if ( $page->post_parent == 0 ) {
+			return $result;
+		} else {
+			return $this->list_pages_at_depth_parent( $page->post_parent, $result );
+		}
 	}
 	
-	if ( $page->post_parent == 0 ) {
-		return $result;
-	} else {
-		return list_pages_at_depth_parent($page->post_parent, $result);
+	/**
+	 * Shortcode [list_pages_at_depth]
+	 */
+	function shortcode_list_pages_at_depth( $atts, $content, $tag ) {
+		$atts['echo'] = 0;
+		$pages = list_pages_at_depth( $atts );
+		$class = isset( $atts['class'] ) ? ' class="list-pages-at-depth ' . $atts['class'] . '"' : 'list-pages-at-depth';
+		return '<ul' . $class . '>' . $pages . '</ul>';
 	}
 
 }
 
-
-
-function shortcode_list_pages_at_depth( $atts, $content, $tag ) {
-	$atts['echo'] = 0;
-	$pages = list_pages_at_depth( $atts );
-	$class = isset( $atts['class'] ) ? ' class="list-pages-at-depth ' . $atts['class'] . '"' : 'list-pages-at-depth';
-	return '<ul' . $class . '>' . $pages . '</ul>';
+/**
+ * List pages at depth
+ */
+function list_pages_at_depth( $args = '' ) {
+	global $List_Pages_At_Depth;
+	return $List_Pages_At_Depth->list_pages( $args );
 }
-
-add_shortcode( 'list-pages-at-depth', 'shortcode_list_pages_at_depth' );
-
-
 
 ?>
