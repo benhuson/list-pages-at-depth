783f7f44ba6f5930631522863da7755ec674edb3
Ben Huson <ben@thewhiteroom.net>
2014-09-19 09:40:09 +0100

    Tidy whitespace and linebreaks.

--- includes/widget.php
+++ includes/widget.php
@@ -1,19 +1,22 @@
 <?php
 
 class WP_Widget_Pages_at_Depth_at_Depth extends WP_Widget {
-	
+
 	/**
 	 * Constructor
 	 */
 	function WP_Widget_Pages_at_Depth_at_Depth() {
+
 		$widget_ops = array( 'classname' => 'widget_pages', 'description' => __( 'Your blog&#8217;s WordPress Pages configured by depth' ) );
 		$this->WP_Widget( 'pages_at_depth', __( 'Pages at Depth' ), $widget_ops );
+
 	}
-	
+
 	/**
 	 * Widget output
 	 */
 	function widget( $args, $instance ) {
+
 		extract( $args );
 
 		$title = apply_filters( 'widget_title', $instance['title'] );
@@ -24,7 +27,7 @@ class WP_Widget_Pages_at_Depth_at_Depth extends WP_Widget {
 
 		if ( $sortby == 'menu_order' )
 			$sortby = 'menu_order, post_title';
-		
+
 		$out = list_pages_at_depth( apply_filters( 'widget_pages_args', array(
 			'startdepth'  => $startdepth,
 			'depth'       => $depth,
@@ -33,7 +36,7 @@ class WP_Widget_Pages_at_Depth_at_Depth extends WP_Widget {
 			'sort_column' => $sortby,
 			'exclude'     => $exclude
 		) ) );
-		
+
 		if ( ! empty( $out ) ) {
 			echo $before_widget;
 			if ( $title )
@@ -41,15 +44,17 @@ class WP_Widget_Pages_at_Depth_at_Depth extends WP_Widget {
 			echo '<ul class="list-pages-at-depth">' . $out . '</ul>';
 			echo $after_widget;
 		}
+
 	}
-	
+
 	/**
 	 * Update Widget
 	 */
 	function update( $new_instance, $old_instance ) {
+
 		$instance = $old_instance;
 		$instance['title'] = strip_tags( $new_instance['title'] );
-		
+
 		if ( in_array( $new_instance['sortby'], array( 'post_title', 'menu_order', 'ID' ) ) ) {
 			$instance['sortby'] = $new_instance['sortby'];
 		} else {
@@ -61,12 +66,14 @@ class WP_Widget_Pages_at_Depth_at_Depth extends WP_Widget {
 		$instance['depth'] = $new_instance['depth'] > 0 ? $new_instance['depth'] : 0;
 
 		return $instance;
+
 	}
-	
+
 	/**
 	 * Widget Form
 	 */
 	function form( $instance ) {
+
 		$instance = wp_parse_args( (array) $instance, array(
 			'sortby'     => 'post_title',
 			'title'      => '',
@@ -76,7 +83,7 @@ class WP_Widget_Pages_at_Depth_at_Depth extends WP_Widget {
 		) );
 		$title = esc_attr( $instance['title'] );
 		$exclude = esc_attr( $instance['exclude'] );
-		
+
 		?>
 		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e( 'Title:' ); ?></label> <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" /></p>
 		<p>
@@ -110,14 +117,15 @@ class WP_Widget_Pages_at_Depth_at_Depth extends WP_Widget {
 			<small><?php _e( 'Page IDs, separated by commas.' ); ?></small>
 		</p>
 		<?php
+
 	}
-	
+
 }
 
 function list_page_at_depth_widgets_init() {
+
 	register_widget( 'WP_Widget_Pages_at_Depth_at_Depth' );
+
 }
 
 add_action( 'widgets_init', 'list_page_at_depth_widgets_init', 1 );
-
-?>
--- list-pages-at-depth.php
+++ list-pages-at-depth.php
@@ -18,61 +18,71 @@ global $List_Pages_At_Depth;
 $List_Pages_At_Depth = new List_Pages_At_Depth();
 
 class List_Pages_At_Depth {
-	
+
 	/**
 	 * Constructor
 	 */
 	function List_Pages_At_Depth() {
+
 		require_once( dirname( __FILE__ ) . '/includes/widget.php' );
+
 		add_shortcode( 'list-pages-at-depth', array( $this, 'shortcode_list_pages_at_depth' ) );
+
 	}
-	
+
 	/**
 	 * List pages
 	 */
 	function list_pages( $args = '' ) {
+
 		global $post;
-		
+
 		if ( ! isset( $args['startdepth'] ) )
 			$args['startdepth'] = 0;
-		
+
 		if ( is_page() || $args['startdepth'] == 0 ) {
 			$result = array();
 			$result = $this->list_pages_at_depth_parent( $post->ID, $result );
-			
+
 			if ( $args['startdepth'] < count( $result ) ) {
 				$args['child_of'] = $result[$args['startdepth']];
 				return wp_list_pages( $args );
 			}
 		}
+
 	}
-	
+
 	/**
 	 * List pages at depth parent
 	 */
 	function list_pages_at_depth_parent( $page_id, $result ) {
+
 		$page = get_page( $page_id );
-		
+
 		if ( ! in_array( $page->ID, $result ) )
 			array_unshift( $result, $page->ID );
 		if ( ! in_array( $page->post_parent, $result ) )
 			array_unshift( $result, $page->post_parent );
-		
+
 		if ( $page->post_parent == 0 ) {
 			return $result;
 		} else {
 			return $this->list_pages_at_depth_parent( $page->post_parent, $result );
 		}
+
 	}
-	
+
 	/**
 	 * Shortcode [list_pages_at_depth]
 	 */
 	function shortcode_list_pages_at_depth( $atts, $content, $tag ) {
+
 		$atts['echo'] = 0;
 		$pages = list_pages_at_depth( $atts );
 		$class = isset( $atts['class'] ) ? ' class="list-pages-at-depth ' . $atts['class'] . '"' : 'list-pages-at-depth';
+
 		return '<ul' . $class . '>' . $pages . '</ul>';
+
 	}
 
 }
@@ -81,8 +91,9 @@ class List_Pages_At_Depth {
  * List pages at depth
  */
 function list_pages_at_depth( $args = '' ) {
-	global $List_Pages_At_Depth;
-	return $List_Pages_At_Depth->list_pages( $args );
-}
 
-?>
+	global $List_Pages_At_Depth;
+
+	return $List_Pages_At_Depth->list_pages( $args );
+
+}
